cdc7a9b400e7331db3206507a6dab4dece203875
Nicolas Zona <nicolas.zona@epitech.eu>
2022-01-11 01:46:51 -0800

    fix(ci): Add procfile
    fix(ci): Fix procfile
    
    fix(ci): app launch
    
    fix(ci): Procfile

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def get_airFrance():
 
 
 # ======== Main ======== #
-if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host="0.0.0.0", port=80)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
