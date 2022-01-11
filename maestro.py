6e264410b36663364589c2901975185dacc4e457
Nicolas Zona <nicolas.zona@epitech.eu>
2022-01-11 00:46:33 -0800

    fix(ci): change port as heroku only handle 80

--- maestro.py
+++ maestro.py
@@ -28,4 +28,4 @@ def get_airFrance():
 
 # ======== Main ======== #
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, use_reloader=True, host="0.0.0.0", port=80)
