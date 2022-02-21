ad9fa29af710ce670f86cb388505cd286a4476a8
Nicolas Zona <nicolas.zona@epitech.eu>
2022-02-21 13:42:59 -0800

    fix(core): Activate multi-thread following a bug API-27

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def get_airFrance():
 
 # ======== Main ======== #
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, threaded=True)
